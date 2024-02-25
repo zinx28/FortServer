f11e26e704959725f057dad49ac99720fdeabaa7
zinx <63958372+zinx28@users.noreply.github.com>
2024-02-25 14:52:02 +0000

    i love you sooooooo

--- FortBackend/src/App/Routes/APIS/Friends/PartyController.cs
+++ FortBackend/src/App/Routes/APIS/Friends/PartyController.cs
@@ -1,6 +1,7 @@
 ﻿using FortBackend.src.App.Utilities.MongoDB.Helpers;
 using FortBackend.src.App.Utilities.MongoDB.Module;
 using Microsoft.AspNetCore.Mvc;
+using System.Text;
 
 namespace FortBackend.src.App.Routes.APIS.Friends
 {
@@ -8,11 +9,18 @@ namespace FortBackend.src.App.Routes.APIS.Friends
     [Route("party")]
     public class PartyController : ControllerBase
     {
-        [HttpGet("api/api/v1/Fortnite/user/:accountId")]
-        public IActionResult FortnitePartyUser(string accountId)
+        [HttpGet("api/v1/Fortnite/user/{accountId}")]
+        public async Task<IActionResult> FortnitePartyUser(string accountId)
         {
             //var UserData = await Handlers.FindOne<User>("accountId", accountId);
+            using (var reader = new StreamReader(Request.Body, Encoding.UTF8))
+            {
+                var requestbody = await reader.ReadToEndAsync();
+                Console.WriteLine("Party " + requestbody);
 
+            }
+
+            return Ok(new { });
         }
     }
 }

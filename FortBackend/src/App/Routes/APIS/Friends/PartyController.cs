3332815fde01cfeb118e5a2946bc40886f3a2803
zinx <63958372+zinx28@users.noreply.github.com>
2024-02-25 15:29:49 +0000

    correct error if invaild user

--- FortBackend/src/App/Routes/APIS/Friends/PartyController.cs
+++ FortBackend/src/App/Routes/APIS/Friends/PartyController.cs
@@ -1,4 +1,6 @@
-﻿using FortBackend.src.App.Utilities.MongoDB.Helpers;
+﻿using FortBackend.src.App.Utilities.Classes.EpicResponses.Errors;
+using FortBackend.src.App.Utilities.Classes.EpicResponses.Profile;
+using FortBackend.src.App.Utilities.MongoDB.Helpers;
 using FortBackend.src.App.Utilities.MongoDB.Module;
 using Microsoft.AspNetCore.Mvc;
 using System.Text;
@@ -12,15 +14,51 @@ namespace FortBackend.src.App.Routes.APIS.Friends
         [HttpGet("api/v1/Fortnite/user/{accountId}")]
         public async Task<IActionResult> FortnitePartyUser(string accountId)
         {
-            //var UserData = await Handlers.FindOne<User>("accountId", accountId);
-            using (var reader = new StreamReader(Request.Body, Encoding.UTF8))
+            var UserData = await Handlers.FindOne<User>("accountId", accountId);
+
+            if (UserData != "Error")
             {
-                var requestbody = await reader.ReadToEndAsync();
-                Console.WriteLine("Party " + requestbody);
 
+
+                using (var reader = new StreamReader(Request.Body, Encoding.UTF8))
+                {
+                    var requestbody = await reader.ReadToEndAsync();
+                    Console.WriteLine("Party " + requestbody);
+
+
+
+                    return Ok(new
+                    {
+
+                    });
+                }
             }
+            /*
+             * 
+             *   throw new BaseError
+                    {
+                        errorCode = "errors.com.epicgames.common.authentication.authentication_failed",
+                        errorMessage = $"Authentication failed for /api/game/v2/profile/{accountId}/{wildcard}/{mcp}",
+                        messageVars = new List<string> { $"/api/game/v2/profile/{accountId}/{wildcard}/{mcp}" },
+                        numericErrorCode = 1032,
+                        originatingService = "any",
+                        intent = "prod",
+                        error_description = $"Authentication failed for /api/game/v2/profile/{accountId}/{wildcard}/{mcp}",
+                    };
+            */
 
-            return Ok(new { });
+
+            return BadRequest(new BaseError
+            {
+                errorCode = "errors.com.epicgames.common.authentication.authentication_failed",
+                errorMessage = $"Authentication failed for /api/v1/Fortnite/user/{accountId}",
+                messageVars = new List<string> { $"/api/v1/Fortnite/user/{accountId}" },
+                numericErrorCode = 1032,
+                originatingService = "party",
+                intent = "prod",
+                error_description = $"Authentication failed for /api/v1/Fortnite/user/{accountId}",
+            });
+
         }
     }
 }

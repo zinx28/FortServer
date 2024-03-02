6ce2e4898528b68a3ac66a6ab11d23d39dc96c04
zinx <63958372+zinx28@users.noreply.github.com>
2024-03-02 23:24:15 +0000

    Create PresenceController.cs

---
+++ FortBackend/src/App/Routes/API/PresenceController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FortBackend.src.App.Routes.API
+{
+    [ApiController]
+    [Route("presence/api/v1")]
+    public class PresenceController : ControllerBase
+    {
+        [HttpGet("_/{accountId}/settings/subscriptions")]
+        public async Task<ActionResult> GrabSettings(string accountId)
+        {
+            Response.ContentType = "application/json";
+            return Ok(Array.Empty<string>());
+        }
+
+        [HttpGet("_/{accountId}/last-online")]
+        public async Task<ActionResult> GrabLastOnline(string accountId)
+        {
+            Response.ContentType = "application/json";
+            return Ok(Array.Empty<string>());
+        }
+
+        [HttpGet("_/{accountId}/subscriptions")]
+        public async Task<ActionResult> SubWow(string accountId)
+        {
+            Response.ContentType = "application/json";
+            return Ok(Array.Empty<string>());
+        }
+
+        [HttpGet("_/{accountId}/subscriptions/nudged")]
+        public async Task<ActionResult> GrabNudged(string accountId)
+        {
+            Response.ContentType = "application/json";
+            return Ok(Array.Empty<string>());
+        }
+    }
+}

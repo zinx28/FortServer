871fd8ea697171c2bc7ad4addc80ee4b354c1f06
zinx <63958372+zinx28@users.noreply.github.com>
2024-03-03 00:06:12 +0000

    Update PartyController.cs

--- FortBackend/src/App/Routes/Friends/PartyController.cs
+++ FortBackend/src/App/Routes/Friends/PartyController.cs
@@ -1,4 +1,5 @@
-﻿using FortBackend.src.App.Utilities.Classes.EpicResponses.Errors;
+﻿using FortBackend.src.App.Utilities;
+using FortBackend.src.App.Utilities.Classes.EpicResponses.Errors;
 using FortBackend.src.App.Utilities.Classes.EpicResponses.Profile;
 using FortBackend.src.App.Utilities.MongoDB.Helpers;
 using FortBackend.src.App.Utilities.MongoDB.Module;
@@ -15,47 +16,80 @@ namespace FortBackend.src.App.Routes.Friends
         [HttpGet("api/v1/Fortnite/user/{accountId}")]
         public async Task<IActionResult> FortnitePartyUser(string accountId)
         {
-            var UserData = await Handlers.FindOne<User>("accountId", accountId);
-
-            if (UserData != "Error")
+            try
             {
+                var UserData = await Handlers.FindOne<User>("accountId", accountId);
 
-                var CurrentParty = GlobalData.parties.Find(e => e.members.Any(a => a == accountId));
-
-                return Ok(new
+                if (UserData != "Error")
                 {
-                    current = CurrentParty != null ? new List<Parties> { CurrentParty } : new List<Parties>(),
-                    pending = Array.Empty<object>(),
-                    invites = Array.Empty<object>(),
-                    pings = Array.Empty<object>()
-                });
-            }
-            /*
-             * 
-             *   throw new BaseError
+
+                    var CurrentParty = GlobalData.parties.Find(e => e.members.Any(a => a == accountId));
+
+                    return Ok(new
+                    {
+                        current = CurrentParty != null ? new List<Parties> { CurrentParty } : new List<Parties>(),
+                        pending = Array.Empty<object>(),
+                        invites = Array.Empty<object>(),
+                        pings = Array.Empty<object>()
+                    });
+                }else
+                {
+                    return BadRequest(new BaseError
                     {
                         errorCode = "errors.com.epicgames.common.authentication.authentication_failed",
-                        errorMessage = $"Authentication failed for /api/game/v2/profile/{accountId}/{wildcard}/{mcp}",
-                        messageVars = new List<string> { $"/api/game/v2/profile/{accountId}/{wildcard}/{mcp}" },
+                        errorMessage = $"Authentication failed for /api/v1/Fortnite/user/{accountId}",
+                        messageVars = new List<string> { $"/api/v1/Fortnite/user/{accountId}" },
                         numericErrorCode = 1032,
-                        originatingService = "any",
+                        originatingService = "party",
                         intent = "prod",
-                        error_description = $"Authentication failed for /api/game/v2/profile/{accountId}/{wildcard}/{mcp}",
-                    };
-            */
-
+                        error_description = $"Authentication failed for /api/v1/Fortnite/user/{accountId}",
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("PartyUserController: " + ex.Message);
+            }
 
             return BadRequest(new BaseError
             {
-                errorCode = "errors.com.epicgames.common.authentication.authentication_failed",
-                errorMessage = $"Authentication failed for /api/v1/Fortnite/user/{accountId}",
-                messageVars = new List<string> { $"/api/v1/Fortnite/user/{accountId}" },
+                errorCode = "errors.com.epicgames.iforgot",
+                errorMessage = $"NGL for /party/api/v1/Fortnite/user/{accountId}",
+                messageVars = new List<string> { $"/party/api/v1/Fortnite/user/{accountId}" },
                 numericErrorCode = 1032,
                 originatingService = "party",
                 intent = "prod",
                 error_description = $"Authentication failed for /api/v1/Fortnite/user/{accountId}",
             });
+        }
 
+        [HttpPost("api/v1/Fortnite/parties")]
+        public async Task<IActionResult> FortniteParty()
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
+                {
+                    string requestBody = await reader.ReadToEndAsync();
+                    Console.WriteLine(requestBody);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("FortniteParty: " + ex.Message);
+            }
+
+            return BadRequest(new BaseError
+            {
+                errorCode = "errors.com.epicgames.iforgot",
+                errorMessage = $"NGL for /party/api/v1/Fortnite/parties",
+                messageVars = new List<string> { $"/party/api/v1/Fortnite/parties" },
+                numericErrorCode = 1032,
+                originatingService = "party",
+                intent = "prod",
+                error_description = $"NGL for /party/api/v1/Fortnite/parties",
+            });
         }
     }
 }

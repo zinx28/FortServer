dd7afd826da6a35bf8107294d015669846ffd8a4
zinx <63958372+zinx28@users.noreply.github.com>
2024-03-09 22:45:26 +0000

    Create LauncherController.cs

---
+++ FortBackend/src/App/Routes/API/LauncherController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FortBackend.src.App.Routes.API
+{
+    //launcher/api/public/distributionpoints
+    [ApiController]
+    [Route("launcher/api")]
+    public class LauncherController : ControllerBase
+    {
+        [HttpGet("public/distributionpoints")]
+        public IActionResult DistributionPoints()
+        {
+            return Ok(new
+            {
+                  distributions = new string[]
+                  {
+                    "https://download.epicgames.com/",
+                    "https://epicgames-download1.akamaized.net/",
+                    "https://fastly-download.epicgames.com/"
+                  }
+            });
+        }
+    }
+}

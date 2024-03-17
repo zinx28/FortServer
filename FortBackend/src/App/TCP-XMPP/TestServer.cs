b3fefe16797a5ca29a484ef722b8f85bc3ac38c8
zinx <63958372+zinx28@users.noreply.github.com>
2024-03-17 19:34:06 +0000

    i will redo this agian

--- FortBackend/src/App/TCP-XMPP/TestServer.cs
+++ FortBackend/src/App/TCP-XMPP/TestServer.cs
@@ -63,43 +63,58 @@ namespace FortBackend.src.App.XMPP
                 //byte[] buffer = new byte[0];
                 XDocument xmlDoc;
                 // NetworkStream stream = client.GetStream();
-
-                using (NetworkStream stream = client.GetStream())
+                string logFilePath = "test.txt";
+                using (StreamWriter sw = File.AppendText(logFilePath))
                 {
-                    byte[] buffer = new byte[1024];
-                    int bytesRead;
-
-                    while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                    using (NetworkStream stream = client.GetStream())
                     {
-                        string receivedMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-                        Console.WriteLine($"Received message: {receivedMessage}");
-
-                        if (receivedMessage.Contains("stream:stream"))
+                        byte[] buffer = new byte[1024];
+                        int totalBytesRead = 0;
+                        while (true)
                         {
-                            string responseXml1 = "<?xml version='1.0' encoding='UTF-8'?>" +
-                        "<stream:stream xmlns:stream='http://etherx.jabber.org/streams' " +
-                        $"xmlns='jabber:client' from='127.0.0.1'  id='{clientId}' " +
-                        "xml:lang='und' version='1.0'>";
+                            int bytesRead = await stream.ReadAsync(buffer, totalBytesRead, buffer.Length - totalBytesRead);
+                            if (bytesRead == 0)
+                            {
+                                break;
+                            }
+                            totalBytesRead += bytesRead;
+                            string receivedMessage = Encoding.UTF8.GetString(buffer, 0, totalBytesRead);
+                            //receivedMessageBuilder.Append(Encoding.UTF8.GetString(buffer, 0, bytesRead));
+                            if (receivedMessage.EndsWith(">"))
+                            {
+                                sw.WriteLine($"Received message: {receivedMessage}");
+                                Console.WriteLine($"Received message: {receivedMessage}");
+
+                                if (receivedMessage.Contains("stream:stream"))
+                                {
+                                    string responseXml1 = "<?xml version='1.0' encoding='UTF-8'?>" +
+                                "<stream:stream xmlns:stream='http://etherx.jabber.org/streams' " +
+                                $"xmlns='jabber:client' from='127.0.0.1'  id='{clientId}' " +
+                                "xml:lang='und' version='1.0'>";
 
 
-                            byte[] responseBytes1 = Encoding.UTF8.GetBytes(responseXml1);
-                            await stream.WriteAsync(responseBytes1, 0, responseBytes1.Length);
-                            Console.WriteLine("Response sent");
-                            await Task.Delay(100);
-                            string responseXml2 = "<stream:features><starttls xmlns='urn:ietf:params:xml:ns:xmpp-tls'><required/></starttls>" +
-                                "<mechanisms xmlns='urn:ietf:params:xml:ns:xmpp-sasl'><mechanism>PLAIN</mechanism></mechanisms></stream:features>";
+                                    byte[] responseBytes1 = Encoding.UTF8.GetBytes(responseXml1);
+                                    await stream.WriteAsync(responseBytes1, 0, responseBytes1.Length);
+                                    Console.WriteLine("Response sent");
+                                    await Task.Delay(100);
+                                    string responseXml2 = "<stream:features><starttls xmlns='urn:ietf:params:xml:ns:xmpp-tls'><required/></starttls>" +
+                                        "<mechanisms xmlns='urn:ietf:params:xml:ns:xmpp-sasl'><mechanism>PLAIN</mechanism></mechanisms></stream:features>";
 
 
-                            byte[] responseBytes2 = Encoding.UTF8.GetBytes(responseXml2);
-                            await stream.WriteAsync(responseBytes2, 0, responseBytes2.Length);
-                            Console.WriteLine("Response sent");
-                        }
-                        else if (receivedMessage.Contains("starttls"))
-                        {
-                            string proceedXml = "<proceed xmlns='urn:ietf:params:xml:ns:xmpp-tls'/>";
-                            byte[] proceedBytes = Encoding.UTF8.GetBytes(proceedXml);
-                            await stream.WriteAsync(proceedBytes, 0, proceedBytes.Length);
-                            Console.WriteLine("Proceed sent");
+                                    byte[] responseBytes2 = Encoding.UTF8.GetBytes(responseXml2);
+                                    await stream.WriteAsync(responseBytes2, 0, responseBytes2.Length);
+                                    Console.WriteLine("Response sent");
+                                }
+                                else if (receivedMessage.Contains("starttls"))
+                                {
+                                    string proceedXml = "<proceed xmlns='urn:ietf:params:xml:ns:xmpp-tls'/>";
+                                    byte[] proceedBytes = Encoding.UTF8.GetBytes(proceedXml);
+                                    await stream.WriteAsync(proceedBytes, 0, proceedBytes.Length);
+                                    Console.WriteLine("Response sent");
+
+                                }
+                                totalBytesRead = 0;
+                            }
                         }
                     }
                 }

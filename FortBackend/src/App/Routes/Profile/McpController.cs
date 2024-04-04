905efc975240dc459f058183ca4dfcc41844cd90
zinx <63958372+zinx28@users.noreply.github.com>
2024-04-04 11:47:25 +0100

    dedicated server

--- FortBackend/src/App/Routes/Profile/McpController.cs
+++ FortBackend/src/App/Routes/Profile/McpController.cs
@@ -34,66 +34,43 @@ namespace FortBackend.src.App.Routes.Profile
             {
                 var RVN = int.Parse(Request.Query["rvn"].FirstOrDefault() ?? "-1");
                 var ProfileID = Request.Query["profileId"].ToString() ?? "athena";
-                var tokenArray = Request.Headers["Authorization"].ToString().Split("bearer ");
-                var token = tokenArray.Length > 1 ? tokenArray[1] : "";
 
-                bool FoundAccount = GlobalData.AccessToken.Any(e => e.token == token) ||
-                    GlobalData.ClientToken.Any(e => e.token == token) ||
-                    GlobalData.RefreshToken.Any(e => e.token == token);
-
-                if (FoundAccount)
+                using (var reader = new StreamReader(Request.Body, Encoding.UTF8))
                 {
-                    ProfileCacheEntry profileCacheEntry = await GrabData.Profile(accountId);
+                    var requestbody = await reader.ReadToEndAsync();
+                    //Console.WriteLine(requestbody);
+                    VersionClass Season = await SeasonUserAgent(Request);
 
-                    var response = new Mcp();
-                    if (profileCacheEntry != null && !string.IsNullOrEmpty(profileCacheEntry.AccountId))
+                    if (string.IsNullOrEmpty(requestbody))
                     {
-                        using (var reader = new StreamReader(Request.Body, Encoding.UTF8))
+                        throw new BaseError
                         {
-                            var requestbody = await reader.ReadToEndAsync();
-                            //Console.WriteLine(requestbody);
-                            VersionClass Season = await SeasonUserAgent(Request);
-                            if (string.IsNullOrEmpty(requestbody))
-                            {
-                                throw new BaseError
-                                {
-                                    errorCode = "errors.com.epicgames.common.iforgot",
-                                    errorMessage = $"No Body for /api/game/v2/profile/{accountId}/{wildcard}/{mcp}",
-                                    messageVars = new List<string> { $"/api/game/v2/profile/{accountId}/{wildcard}/{mcp}" },
-                                    numericErrorCode = 1032,
-                                    originatingService = "any",
-                                    intent = "prod",
-                                    error_description = $"No Body for /api/game/v2/profile/{accountId}/{wildcard}/{mcp}",
-                                };
-                            }
+                            errorCode = "errors.com.epicgames.common.iforgot",
+                            errorMessage = $"No Body for /api/game/v2/profile/{accountId}/{wildcard}/{mcp}",
+                            messageVars = new List<string> { $"/api/game/v2/profile/{accountId}/{wildcard}/{mcp}" },
+                            numericErrorCode = 1032,
+                            originatingService = "any",
+                            intent = "prod",
+                            error_description = $"No Body for /api/game/v2/profile/{accountId}/{wildcard}/{mcp}",
+                        };
+                    }
+                    var response = new Mcp();
+                    if (wildcard == "dedicated_server")
+                    {
+                        // we check from the cached data... it will no matter try to grab the data if the account id is valid
+                        ProfileCacheEntry profileCacheEntry = await GrabData.Profile(accountId);
+
+                        if (profileCacheEntry != null && !string.IsNullOrEmpty(profileCacheEntry.AccountId))
+                        {
+
                             switch (mcp)
                             {
                                 case "QueryProfile":
                                     response = await QueryProfile.Init(accountId, ProfileID, Season, RVN, profileCacheEntry);
                                     break;
-                                case "ClientQuestLogin":
-                                    response = await ClientQuestLogin.Init(accountId, ProfileID, Season, RVN, profileCacheEntry);
-                                    break;
-                                case "SetCosmeticLockerSlot":
-                                    response = await SetCosmeticLockerSlot.Init(accountId, ProfileID, Season, RVN, profileCacheEntry, JsonConvert.DeserializeObject<SetCosmeticLockerSlotRequest>(requestbody)!);
-                                    break;
-                                case "EquipBattleRoyaleCustomization":
-                                    response = await EquipBattleRoyaleCustomization.Init(accountId, ProfileID, Season, RVN, profileCacheEntry, JsonConvert.DeserializeObject<EquipBattleRoyaleCustomizationRequest>(requestbody)!);
-                                    break;
-                                case "SetBattleRoyaleBanner":
-                                    response = await SetBattleRoyaleBanner.Init(accountId, ProfileID, Season, RVN, profileCacheEntry, JsonConvert.DeserializeObject<SetBattleRoyaleBannerReq>(requestbody)!);
-                                    break;
-                                case "PurchaseCatalogEntry":
-                                    response = await PurchaseCatalogEntry.Init(accountId, ProfileID, Season, RVN, profileCacheEntry, JsonConvert.DeserializeObject<PurchaseCatalogEntryRequest>(requestbody)!);
-                                    break;
-                                case "BulkEquipBattleRoyaleCustomization":
-                                    response = await BulkEquipBattleRoyaleCustomization.Init(accountId, ProfileID, Season, RVN, profileCacheEntry, JsonConvert.DeserializeObject<BulkEquipBattleRoyaleCustomizationResponse>(requestbody)!);
-                                    break; // not proper
-                                //case "CopyCosmeticLoadout":
-                                //    response = await CopyCosmeticLoadout.Init(accountId, ProfileID, Season, RVN, profileCacheEntry, JsonConvert.DeserializeObject<CopyCosmeticLoadoutResponse>(requestbody));
-                                //    break;
-                                default:
 
+                                default:
+                                    Logger.Error("MISSING MCP REQUEST FOR DEDICATED SERVER -> " + mcp);
                                     response = new Mcp
                                     {
                                         profileRevision = ProfileID == "common_core" || ProfileID == "common_public" ? profileCacheEntry.AccountData.commoncore.RVN : profileCacheEntry.AccountData.athena.RVN,
@@ -105,25 +82,98 @@ namespace FortBackend.src.App.Routes.Profile
                                     };
                                     break;
                             }
-                        }
 
-                        return response;
+                            return response;
+                        }
+                        else
+                        {
+                            throw new BaseError
+                            {
+                                errorCode = "errors.com.epicgames.common.authentication.authentication_failed",
+                                errorMessage = $"Authentication failed for /api/game/v2/profile/{accountId}/{wildcard}/{mcp}",
+                                messageVars = new List<string> { $"/api/game/v2/profile/{accountId}/{wildcard}/{mcp}" },
+                                numericErrorCode = 1032,
+                                originatingService = "any",
+                                intent = "prod",
+                                error_description = $"Authentication failed for /api/game/v2/profile/{accountId}/{wildcard}/{mcp}",
+                            };
+                        }
+                    }
+                    else
+                    {
+                        var tokenArray = Request.Headers["Authorization"].ToString().Split("bearer ");
+                        var token = tokenArray.Length > 1 ? tokenArray[1] : "";
+
+                        bool FoundAccount = GlobalData.AccessToken.Any(e => e.token == token) ||
+                            GlobalData.ClientToken.Any(e => e.token == token) ||
+                            GlobalData.RefreshToken.Any(e => e.token == token);
+
+                        if (FoundAccount)
+                        {
+                            ProfileCacheEntry profileCacheEntry = await GrabData.Profile(accountId);
+
+                            if (profileCacheEntry != null && !string.IsNullOrEmpty(profileCacheEntry.AccountId))
+                            {
+
+
+                                switch (mcp)
+                                {
+                                    case "QueryProfile":
+                                        response = await QueryProfile.Init(accountId, ProfileID, Season, RVN, profileCacheEntry);
+                                        break;
+                                    case "ClientQuestLogin":
+                                        response = await ClientQuestLogin.Init(accountId, ProfileID, Season, RVN, profileCacheEntry);
+                                        break;
+                                    case "SetCosmeticLockerSlot":
+                                        response = await SetCosmeticLockerSlot.Init(accountId, ProfileID, Season, RVN, profileCacheEntry, JsonConvert.DeserializeObject<SetCosmeticLockerSlotRequest>(requestbody)!);
+                                        break;
+                                    case "EquipBattleRoyaleCustomization":
+                                        response = await EquipBattleRoyaleCustomization.Init(accountId, ProfileID, Season, RVN, profileCacheEntry, JsonConvert.DeserializeObject<EquipBattleRoyaleCustomizationRequest>(requestbody)!);
+                                        break;
+                                    case "SetBattleRoyaleBanner":
+                                        response = await SetBattleRoyaleBanner.Init(accountId, ProfileID, Season, RVN, profileCacheEntry, JsonConvert.DeserializeObject<SetBattleRoyaleBannerReq>(requestbody)!);
+                                        break;
+                                    case "PurchaseCatalogEntry":
+                                        response = await PurchaseCatalogEntry.Init(accountId, ProfileID, Season, RVN, profileCacheEntry, JsonConvert.DeserializeObject<PurchaseCatalogEntryRequest>(requestbody)!);
+                                        break;
+                                    case "BulkEquipBattleRoyaleCustomization":
+                                        response = await BulkEquipBattleRoyaleCustomization.Init(accountId, ProfileID, Season, RVN, profileCacheEntry, JsonConvert.DeserializeObject<BulkEquipBattleRoyaleCustomizationResponse>(requestbody)!);
+                                        break; // not proper
+                                               //case "CopyCosmeticLoadout":
+                                               //    response = await CopyCosmeticLoadout.Init(accountId, ProfileID, Season, RVN, profileCacheEntry, JsonConvert.DeserializeObject<CopyCosmeticLoadoutResponse>(requestbody));
+                                               //    break;
+                                    default:
+
+                                        response = new Mcp
+                                        {
+                                            profileRevision = ProfileID == "common_core" || ProfileID == "common_public" ? profileCacheEntry.AccountData.commoncore.RVN : profileCacheEntry.AccountData.athena.RVN,
+                                            profileId = ProfileID,
+                                            profileChangesBaseRevision = ProfileID == "common_core" || ProfileID == "common_public" ? profileCacheEntry.AccountData.commoncore.RVN : profileCacheEntry.AccountData.athena.RVN,
+                                            profileCommandRevision = ProfileID == "common_core" || ProfileID == "common_public" ? profileCacheEntry.AccountData.commoncore.CommandRevision : profileCacheEntry.AccountData.athena.CommandRevision,
+                                            serverTime = DateTime.Parse(DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss.fffZ")),
+                                            responseVersion = 1
+                                        };
+                                        break;
+                                }
+                            }
+
+                            return response;
+                        }
+                        else
+                        {
+                            throw new BaseError
+                            {
+                                errorCode = "errors.com.epicgames.common.authentication.authentication_failed",
+                                errorMessage = $"Authentication failed for /api/game/v2/profile/{accountId}/{wildcard}/{mcp}",
+                                messageVars = new List<string> { $"/api/game/v2/profile/{accountId}/{wildcard}/{mcp}" },
+                                numericErrorCode = 1032,
+                                originatingService = "any",
+                                intent = "prod",
+                                error_description = $"Authentication failed for /api/game/v2/profile/{accountId}/{wildcard}/{mcp}",
+                            };
+                        }
                     }
                 }
-                else
-                {
-                    throw new BaseError
-                    {
-                        errorCode = "errors.com.epicgames.common.authentication.authentication_failed",
-                        errorMessage = $"Authentication failed for /api/game/v2/profile/{accountId}/{wildcard}/{mcp}",
-                        messageVars = new List<string> { $"/api/game/v2/profile/{accountId}/{wildcard}/{mcp}" },
-                        numericErrorCode = 1032,
-                        originatingService = "any",
-                        intent = "prod",
-                        error_description = $"Authentication failed for /api/game/v2/profile/{accountId}/{wildcard}/{mcp}",
-                    };
-                }
-
             }
             catch (BaseError ex)
             {

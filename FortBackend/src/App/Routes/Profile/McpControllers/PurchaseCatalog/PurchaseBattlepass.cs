dfd65e4a3b7be8dbec95345106eeec907004a919
zinx <63958372+zinx28@users.noreply.github.com>
2024-05-11 16:18:22 +0100

    lil fix

--- FortBackend/src/App/Routes/Profile/McpControllers/PurchaseCatalog/PurchaseBattlepass.cs
+++ FortBackend/src/App/Routes/Profile/McpControllers/PurchaseCatalog/PurchaseBattlepass.cs
@@ -191,9 +191,9 @@ namespace FortBackend.src.App.Routes.Profile.McpControllers.PurchaseCatalog
                                                                             });
                                                                         }
 
-                                                                        seasonObject.Quests.Add($"{PaidBundles.templateId}", new DailyQuestsData
+                                                                        seasonObject.Quests.Add($"Quest:{PaidBundles.templateId}", new DailyQuestsData
                                                                         {
-                                                                            templateId = $"{PaidBundles.templateId}",
+                                                                            templateId = $"Quest:{PaidBundles.templateId}",
                                                                             attributes = new DailyQuestsDataDB
                                                                             {
                                                                                 challenge_bundle_id = $"ChallengeBundle:{kvp.BundleId}",
@@ -206,7 +206,7 @@ namespace FortBackend.src.App.Routes.Profile.McpControllers.PurchaseCatalog
 
                                                                         var ItemObjectResponse = new
                                                                         {
-                                                                            templateId = $"{PaidBundles.templateId}",
+                                                                            templateId = $"Quest:{PaidBundles.templateId}",
                                                                             attributes = new Dictionary<string, object>
                                                                             {
                                                                                 { "creation_time", DateTime.Now.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ") },
@@ -239,7 +239,7 @@ namespace FortBackend.src.App.Routes.Profile.McpControllers.PurchaseCatalog
                                                                         ApplyProfileChanges.Add(new MultiUpdateClass
                                                                         {
                                                                             changeType = "itemAdded",
-                                                                            itemId = $"{PaidBundles.templateId}",
+                                                                            itemId = $"Quest:{PaidBundles.templateId}",
                                                                             item = ItemObjectResponse
                                                                         });
                                                                     }
